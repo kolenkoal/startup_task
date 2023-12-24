915e5ccd4f863f72679d8b4ddf9cb11bc3d28293
kolenkoal <kolenko.alexander0@gmail.com>
2023-12-24 12:31:47 +0300

    Added functionality to delete old links

--- app/links/admin.py
+++ app/links/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils import timezone
+from .models import Link
 
-# Register your models here.
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'last_accessed')
+    actions = ['delete_old_links']
+
+    @admin.action(description="Delete links older than 2 weeks")
+    def delete_old_links(self, request, queryset):
+        two_weeks_ago = timezone.now() - timezone.timedelta(weeks=2)
+        old_links = queryset.filter(last_accessed__lt=two_weeks_ago)
+        deleted_count, _ = old_links.delete()
+        self.message_user(request, f"Deleted {deleted_count} old links.")
